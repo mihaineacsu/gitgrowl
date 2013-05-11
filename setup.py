b2c0f24cd1a7c9a2c7b75cb7d79d335a06cb9701
mihaineacsu <mihai.neacsu@ymail.com>
2013-05-11 04:34:01 +0300

    Add and configure setup.py for installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('./README.md', 'r') as readme_file:
+    readme_description = readme_file.read()
+
+setup(
+    name='gitgrowl',
+    version='0.1',
+    author='Mihai Neacsu',
+    author_email='mihai.neacsu@ymail.com',
+    url='https://github.com/mihaineacsu/gitgrowl',
+	packages = ['gitgrowl'],
+    download_url='https://github.com/mihaineacsu/gitgrowl',
+    description='Github repository activity aggregator',
+    long_description=readme_description,
+    platforms='any',
+	entry_points={
+		'console_scripts': [
+			'gitgrowl = gitgrowl.main:main',
+			]
+		},
+)
