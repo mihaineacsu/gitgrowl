0d962c48f87c221199eee283f5e60a071c7577d9
mihaineacsu <mihai.neacsu@ymail.com>
2013-05-11 21:08:58 +0300

    Add implementation to read/use config file if it exists

--- gitgrowl/events.py
+++ gitgrowl/events.py
@@ -3,25 +3,30 @@
 from gitgrowl import os
 from gitgrowl import sqlite3
 from gitgrowl import default_config
+import ast
 
 conn = None
+config = None
 
 def setup_env():
 	config = default_config.config
 
 	with open('.gitgrowl_config', 'w+') as config_file:
-		config_file.write(''.join(('config=', repr(config))))
+		config_file.write(''.join((repr(config))))
 
 	db_file = config['db_file']
 	conn = sqlite3.connect(db_file)
 
 	with open('.gitignore', 'a+') as gitignore:
 		gitignore.write('\n# gitgrowl\n')
-		gitignore.write(''.join(('.gitgrowl_config\n', db_file)))
+		gitignore.write(''.join(('.gitgrowl_config\n', db_file, '\n')))
 
 def check_db():
 	if not os.path.isfile(''.join((os.getcwd(), '/.gitgrowl_config'))):
-	   setup_env() 
+		setup_env()
+	else:
+		config = ast.literal_eval(open('.gitgrowl_config', 'r').read())
+		conn = sqlite3.connect(config['db_file'])
 
 def check_events():
 	check_db()
