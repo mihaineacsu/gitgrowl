c33ba2a020a9953d4df9f506a38b262e30d5f3f0
mihaineacsu <mihai.neacsu@ymail.com>
2013-05-11 05:03:51 +0300

    Add main.py and make it runnable

--- gitgrowl/main.py
+++ gitgrowl/main.py
@@ -1,2 +1,23 @@
+#!/usr/bin/python
+
+import sys
+
+usage = 'Usage: '
+
+def run_events():
+	pass
+
+def run_stats():
+	pass
+
+def run_command(command):
+	return {
+		'events': run_events(),
+		'stats' : run_stats(),
+	}.get(command, usage)
+
 def main():
-	print 'Ready'
+	if len(sys.argv) == 1:
+		print usage
+	else:
+		run_command(sys.argv[1])
