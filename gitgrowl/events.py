df3635625e44c16f8bd992583b46191cce6ed345
mihaineacsu <mihai.neacsu@ymail.com>
2013-05-12 15:54:06 +0300

    Add code to find remote origin

--- gitgrowl/events.py
+++ gitgrowl/events.py
@@ -3,6 +3,7 @@
 import os
 import ast
 import sqlite3
+import subprocess
 from gitgrowl import default_config
 
 conn = None
@@ -14,7 +15,8 @@ def setup_env():
 	with open('.gitgrowl_config', 'w+') as config_file:
 		config_file.write(''.join((repr(config))))
 
-	conn = sqlite3.connect(config['db_file'])
+	db_file = config['db_file']
+	conn = sqlite3.connect(db_file)
 
 	with open('.gitignore', 'a+') as gitignore:
 		gitignore.write('\n# gitgrowl\n')
@@ -27,8 +29,14 @@ def check_db():
 		config = ast.literal_eval(open('.gitgrowl_config', 'r').read())
 		conn = sqlite3.connect(config['db_file'])
 
+def get_repo():
+	output = subprocess.check_output(["git", "remote", "-v"])
+	return output.split()[1][:-4].split('/', 3)[3] 
+
 def check_events():
 	check_db()
+	repo = get_repo()
+	
 
 def events_main(args):
 	if len(args) == 1:
